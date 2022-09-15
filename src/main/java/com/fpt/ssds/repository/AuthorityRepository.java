c4b5a5bfda62a9b0f655b895259d4258184d70ff
trinh.pham <trinh.pham@onemount.com>
2022-09-15 12:18:16 +0700

    Initial version of SSDS generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/fpt/ssds/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.fpt.ssds.repository;
+
+import com.fpt.ssds.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/fpt/ssds/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,18 @@
+package com.fpt.ssds.security;
+
+import com.fpt.ssds.config.Constants;
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link AuditorAware} based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM));
+    }
+}
